fdeaa579be82762603e379358109ec4168374a39
Guilherme Ramalho <guilherme-rarmalho@outlook.com>
2019-09-04 11:37:26 -0300

    Adicionad migration da tabela users

--- src/app.js
+++ src/app.js
@@ -4,15 +4,7 @@ import routes from "./routes";
 class App {
   constructor() {
     this.server = express();
-    this.middlewares();
-    this.routes();
-  }
-
-  middlewares() {
     this.server.use(express.json());
-  }
-
-  routes() {
     this.server.use(routes);
   }
 }
